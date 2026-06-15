26f60c388795616353654bcdb6d824ee4cb0572b
Level237 <bramslevel129@gmail.com>
2026-06-15 14:42:36 +0100

    feat: implement onboarding feature with associated assets, constants, and data structures

---
+++ src/features/welcome/constants/images.ts
@@ -0,0 +1,14 @@
+export const imagesOnboarding = [
+    {
+        id: 1,
+        image: require("@/assets/images/onboarding/onboarding1.png"),
+    },
+    {
+        id: 2,
+        image: require("@/assets/images/onboarding/onboarding2.png"),
+    },
+    {
+        id: 3,
+        image: require("@/assets/images/onboarding/onboarding3.jpg"),
+    },
+];  
